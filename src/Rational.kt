aa4a653e315ce9753a1426936cad32543e20f7f3
Koya Fukuda <kouyaf77@gmail.com>
2018-04-23 22:20:08 +0900

    Created  Rational class

---
+++ src/Rational.kt
@@ -0,0 +1,21 @@
+class Rational(n: Int, val d: Int) {
+    init {
+        require(d != 0, {"denominator must not be null"})
+    }
+    private val g by lazy {gcd(Math.abs(n), Math.abs(d))}
+    val numerator: Int by lazy {n / g}
+    val denominator: Int by lazy {d / g}
+    operator fun plus(that: Rational): Rational =
+            Rational(
+                    numerator * that.denominator + that.numerator * denominator,
+                    denominator * that.denominator
+            )
+    operator fun plus(n: Int): Rational =
+            Rational(numerator + n * denominator, denominator)
+    override fun toString(): String = "${numerator}/${denominator}"
+    tailrec private fun gcd(a: Int, b: Int): Int =
+            if (b == 0) a
+            else gcd(b, a % b)
+}
+
+operator fun Int.plus(r: Rational): Rational = r + this
